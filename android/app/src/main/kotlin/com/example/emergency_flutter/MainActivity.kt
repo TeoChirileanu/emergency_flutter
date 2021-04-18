98507a2296a24e02ffe18323ba0286b553175f54
Teodor Chirileanu <teodorchirileanu@gmail.com>
2021-04-18 20:28:54 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/emergency_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.emergency_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,39 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyHomePage extends StatefulWidget {
+  final _optiuniDeSelectare = [
+    new DropdownMenuItem(
+      child: Center(child: Text("Inscriere-te ca voluntar")),
+      value: "inscriere",
+    ),
+    new DropdownMenuItem(
+      child: Center(child: Text("Anunta o urgenta")),
+      value: "urgenta",
+    ),
+  ];
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AspectRatio(
+        aspectRatio: 3 / 2,
+        child: Column(
+          children: [
+            AutoSizeText(
+              "Bun venit, ce ați dori să faceți? 🙂",
+              style: GoogleFonts.montserrat(fontSize: 99),
+              maxLines: 1,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
